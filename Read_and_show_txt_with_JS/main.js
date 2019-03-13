136849da745aee22c8a8c3c82decf24bed6ccc99
Lawliet00 <JuanFBass17@gmail.com>
2019-03-13 14:55:50 -0400

    algoritmo para leer con Js un txt y mostrarlos en pantalla con una establecida..

--- Read_and_show_txt_with_JS/main.js
+++ Read_and_show_txt_with_JS/main.js
@@ -21,12 +21,12 @@ function onLoad() {
   lineas.forEach(function(linea){
   	if (linea.split("/").length > 1) {
   	var Linetext = linea.split(".");
-	  	document.getElementById('texto').innerHTML += '<pre>[<br>    "group"=>"'+Linetext[0][1]+'", "subgroup"=>"'+Linetext[1]+'","item"=>"'+Linetext[2]+'", <br>    "account"=>"'+Linetext[3]+'",'+
-	  	 '"first_sub_account"=>"'+Linetext[4]+'", "second_sub_account"=>"'+Linetext[5]+'",<br>    "denomination"=>"'+Linetext[6]+'",<br>    "active"=>true <br>],</pre>';  		
+	  	document.getElementById('texto').innerHTML += '[<br>    "group"=>"'+Linetext[0][1]+'", "subgroup"=>"'+Linetext[1]+'","item"=>"'+Linetext[2]+'", <br>    "account"=>"'+Linetext[3]+'",'+
+	  	 '"first_sub_account"=>"'+Linetext[4]+'", "second_sub_account"=>"'+Linetext[5]+'",<br>    "denomination"=>"'+Linetext[6]+'",<br>    "active"=>false <br>],<br>';  		
   	}else{
 	  	var Linetext = linea.split(".");
-	  	document.getElementById('texto').innerHTML += '<pre>[<br>    "group"=>"'+Linetext[0]+'", "subgroup"=>"'+Linetext[1]+'","item"=>"'+Linetext[2]+'", <br>    "account"=>"'+Linetext[3]+'",'+
-	  	 '"first_sub_account"=>"'+Linetext[4]+'", "second_sub_account"=>"'+Linetext[5]+'",<br>    "denomination"=>"'+Linetext[6]+'"<br>],</pre>';  		
+	  	document.getElementById('texto').innerHTML += '[<br>    "group"=>"'+Linetext[0]+'", "subgroup"=>"'+Linetext[1]+'","item"=>"'+Linetext[2]+'", <br>    "account"=>"'+Linetext[3]+'",'+
+	  	 '"first_sub_account"=>"'+Linetext[4]+'", "second_sub_account"=>"'+Linetext[5]+'",<br>    "denomination"=>"'+Linetext[6]+'"<br>],<br>';  		
 	}  
   });
 }
