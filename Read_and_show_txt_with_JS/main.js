e17ad15c42a5a128ba0ebd60153064ba2c310001
Lawliet00 <JuanFBass17@gmail.com>
2019-03-13 14:48:55 -0400

    algoritmo para leer con Js un txt y mostrarlos en pantalla con una estructura establecida

---
+++ Read_and_show_txt_with_JS/main.js
@@ -0,0 +1,32 @@
+
+var input = myForm.myInput;
+var reader = new FileReader;
+
+input.addEventListener('change', onChange);
+
+
+function onChange(event) {
+  var file = event.target.files[0];
+  
+  reader.readAsText(file);
+  
+  reader.onload = onLoad;
+  
+}
+
+function onLoad() {
+  var result = reader.result;
+  var lineas = result.split('\n');
+  
+  lineas.forEach(function(linea){
+  	if (linea.split("/").length > 1) {
+  	var Linetext = linea.split(".");
+	  	document.getElementById('texto').innerHTML += '<pre>[<br>    "group"=>"'+Linetext[0][1]+'", "subgroup"=>"'+Linetext[1]+'","item"=>"'+Linetext[2]+'", <br>    "account"=>"'+Linetext[3]+'",'+
+	  	 '"first_sub_account"=>"'+Linetext[4]+'", "second_sub_account"=>"'+Linetext[5]+'",<br>    "denomination"=>"'+Linetext[6]+'",<br>    "active"=>true <br>],</pre>';  		
+  	}else{
+	  	var Linetext = linea.split(".");
+	  	document.getElementById('texto').innerHTML += '<pre>[<br>    "group"=>"'+Linetext[0]+'", "subgroup"=>"'+Linetext[1]+'","item"=>"'+Linetext[2]+'", <br>    "account"=>"'+Linetext[3]+'",'+
+	  	 '"first_sub_account"=>"'+Linetext[4]+'", "second_sub_account"=>"'+Linetext[5]+'",<br>    "denomination"=>"'+Linetext[6]+'"<br>],</pre>';  		
+	}  
+  });
+}
